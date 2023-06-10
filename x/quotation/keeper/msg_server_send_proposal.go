a044f1d390e2f7e7c69a1b822d984995b7df6c46
anurag pal <anurag.pal@sukshi.com>
2023-06-10 00:35:35 +0000

    change structure of request quotation

--- x/quotation/keeper/msg_server_send_proposal.go
+++ x/quotation/keeper/msg_server_send_proposal.go
@@ -4,11 +4,17 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"quotation-mvp/x/quotation/types"
 )
 
 func (k msgServer) SendProposal(goCtx context.Context, msg *types.MsgSendProposal) (*types.MsgSendProposalResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	quotation, found := k.GetQuotation(ctx, msg.Tx.RqId)
+	if !found {
+		return nil, sdkerrors.ErrKeyNotFound
+	}
 
 	p := types.Proposal{
 		Id:       msg.Id,
@@ -17,6 +23,13 @@ func (k msgServer) SendProposal(goCtx context.Context, msg *types.MsgSendProposa
 		Version:  msg.Version,
 		Creator:  msg.Creator,
 	}
+
+	var ps []string
+
+	ps = quotation.Tx.Proposals
+	ps = append(ps, msg.Id)
+	quotation.Tx.Proposals = ps
+	k.SetQuotation(ctx, quotation)
 	k.SetProposal(ctx, p)
 
 	return &types.MsgSendProposalResponse{}, nil
